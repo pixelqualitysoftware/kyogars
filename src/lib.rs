c539067269636b48c112afff6d1fe04774f68057
Tuxzilla <tuxzilla@tuxzilla.com>
2026-09-13 12:39:01 -0500

    Initialize project structure
    Signed-off-by: Tuxzilla <tuxzilla@tuxzilla.com>

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+//! # Kyogars
+//!
+//! Parser for Kyogas, A simple markdown language.
+//!
+//! Read more about Kyogas [here](https://github.com/pixelqualitysoftware).
+//!
+//! View our repository [here](https://github.com/pixelqualitysoftware/kyogars)
+//!
+//! Writen by [Tuxzilla T. Penguin](https://tuxzilla.com)
+//! Email for support [here](mailto:tuxzilla@tuxzilla.com)
+
+// src/lib.rs
+
+pub fn load_file() -> String {
+    let string = String::from("curse words");
+    string
+}
